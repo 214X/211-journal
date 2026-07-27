78fb82f6bbec9b6283c1162de9aab83677e4b28a
214X <92593070+214X@users.noreply.github.com>
2026-07-27 20:25:43 +0300

    First commit :)

---
+++ frontend/211-journal/components/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+
+
+export default function HomePage() {
+    return (
+        <div>
+            {/* Name part */}
+            {/* About me */}
+            <h1>Hello world</h1>
+        </div>
+    )
+}
