c3bbb8c611189a4220910c0d8cbc852b2ab4eda6
214X <92593070+214X@users.noreply.github.com>
2026-09-20 20:25:50 +0300

    Contact page added. It is empty temporarily.

---
+++ frontend/211-journal/app/[locale]/contact/page.tsx
@@ -0,0 +1,6 @@
+import ContactPage from "@/components/ContactPage/ContactPage";
+
+
+export default function Page () {
+    return <ContactPage/>;
+}
---
+++ frontend/211-journal/components/ContactPage/ContactPage.tsx
@@ -0,0 +1,11 @@
+import styles from "./ContactPage.module.css";
+
+export default function ContactPage() {
+    return (
+        <section className={styles.contactPageWrapper}>
+            <section className={styles.mainContainer}>
+                This page is temporarily empty.
+            </section>
+        </section>
+    )
+}
