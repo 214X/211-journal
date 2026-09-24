f460df7d51a6749cfbcba9b5c95c85e844fe010e
214X <92593070+214X@users.noreply.github.com>
2026-09-24 14:28:05 +0300

    New feature added: Navbar content changes by language.

--- frontend/211-journal/components/Navbar/Navbar.tsx
+++ frontend/211-journal/components/Navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { useLocale } from "next-intl";
 import { Link, usePathname } from "@/i18n/navigation";
+import { getContent, Locale } from "@/i18n/getContent";
+import navbarContent from "./navbarContent.json";
 import styles from "./Navbar.module.css";
 import ThemeButton from "./ThemeButton/ThemeButton";
 
 export default function Navbar() {
     const pathname = usePathname();
+
+    const locale = useLocale();
+    const t = getContent(navbarContent, locale as Locale);
 
     return (
         <nav className={styles.navbar}>
@@ -13,13 +19,13 @@ export default function Navbar() {
                 href="/"
                 className={pathname === '/' ? styles.active : undefined}
             >
-                Home
+                {t.home}
             </Link>
             <Link
                 href="/contact"
                 className={pathname === '/contact' ? styles.active : undefined}
             >
-                Contact
+                {t.contact}
             </Link>
 
             <ThemeButton></ThemeButton>
